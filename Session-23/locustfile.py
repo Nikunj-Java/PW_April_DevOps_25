4a00947f2f70ee1cb5868d252d2132df9d10765b
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-08-02 20:48:15 +0530

    Locust

---
+++ Session-23/locustfile.py
@@ -0,0 +1,9 @@
+from locust import HttpUser,task,between
+
+class JsonPlaceholderUser(HttpUser):
+    wait_time=between(1,2)
+    host="https://jsonplaceholder.typicode.com"
+    
+    @task
+    def getUsers(self):
+        self.client.get("/users")
