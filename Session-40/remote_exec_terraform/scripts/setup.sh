f8285f77494f96162ee00313315c14bb7ce4aa34
Nikunj-Java <ganitniduniyanikunj10@gmail.com>
2025-10-11 21:11:16 +0530

    terraform Infra remote and server

--- Session-40/remote_exec_terraform/scripts/setup.sh
+++ Session-40/remote_exec_terraform/scripts/setup.sh
@@ -7,13 +7,19 @@ if [ -f "$MARKER_FILE" ]; then
   exit 0
 fi
 
-echo "Updating system and installing Nginx..."
+echo "Updating system..."
 yum update -y
+
+echo "Installing Nginx..."
+amazon-linux-extras enable nginx1
 yum install -y nginx
 
+echo "Starting Nginx..."
 systemctl enable nginx
 systemctl start nginx
 
+# Create index.html safely
+mkdir -p /usr/share/nginx/html
 echo "<h1>Hello from Terraform Provisioner!</h1>" > /usr/share/nginx/html/index.html
 
 touch "$MARKER_FILE"
